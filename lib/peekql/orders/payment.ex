e92c91c9aa50eb9ad9592149402134da12dbef86
Emilio <thecraftedgem@tuta.io>
2019-09-06 11:40:42 -0600

    Fixed Ecto Relationships

--- lib/peekql/orders/payment.ex
+++ lib/peekql/orders/payment.ex
@@ -1,20 +1,27 @@
 defmodule Peekql.Orders.Payment do
   use Ecto.Schema
   import Ecto.Changeset
-
+  import Ecto.Query
+  alias Peekql.Repo
 
   schema "payments" do
     field :amount, :integer
     field :note, :string
-    field :order_id, :id
-    belongs_to :orders, Order
+    belongs_to :order, Peekql.Orders.Order
     timestamps()
   end
 
   @doc false
   def changeset(payment, attrs) do
     payment
-    |> cast(attrs, [])
+    |> cast(attrs, [:amount, :note])
     |> validate_required([])
   end
+
+  def index do
+    query = from p in Peekql.Orders.Payment,
+              inner_join: o in Peekql.Orders.Order, on: p.order_id == o.id
+    Repo.all(query)
+  end
+
 end
